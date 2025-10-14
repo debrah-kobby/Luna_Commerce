61d04e66e7abf16856e839555c69cf986f07959f
Prince Sam <bessahsam0277@gmail.com>
2025-10-14 01:44:36 +0000

    Mods Upon Mod

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ let loaderContainer = document.querySelector(".loader-container");
 console.log("script loaded");
 
 // Hide body content at the start
-/* document.body.style.overflow = "hidden"; // prevents scrolling */
+document.body.style.overflow = "hidden"; // prevents scrolling
 document.querySelector(".first_page").style.display = "none";
 
 window.addEventListener("load", () => {
   setTimeout(() => {
     loaderContainer.style.display = "none"; // Hide loader
     document.querySelector(".first_page").style.display = "block"; // Show main content
-    /* document.body.style.overflow = "auto"; // allow scrolling again */
+    document.body.style.overflow = "auto"; // allow scrolling again
     console.log("Loader-Hidden");
   }, 1000); // 5 seconds
 });
